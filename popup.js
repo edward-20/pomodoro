ec17011eadbf7c784f34ed4184592cf2232a8206
Edward Lim <ed.lim199@gmail.com>
2024-02-11 14:10:13 +1100

    basic functioning

--- popup.js
+++ popup.js
@@ -1,4 +1,28 @@
 const button = document.querySelector('button');
 button.addEventListener('click', async () => {
+  setInterval(() => {
+    let minute = document.getElementById("minute").value;
+    let seconds = document.getElementById("seconds").value;
+    
+    if (seconds === "0") {
+      // if we've finished the whole time
+      if (minute === "0") {
+        // show the finished banner
+        document.getElementById("done-banner").classList.remove('d-none');
+        // make a sound
+        document.getElementById("alarm-sound").play();
+      } 
+      // if we've finished the minute
+      else {
+        minute--;
+        seconds = "59";
+      }
+    } 
+    else {
+      seconds--;
+    }
 
+    document.getElementById("minute").value = minute;
+    document.getElementById("seconds").value = seconds;
+  }, 1000)
 });
